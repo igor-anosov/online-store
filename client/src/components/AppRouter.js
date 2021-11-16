7cea7e3bfcea8ae083c026fd00a960f78bc43bf2
igor-anosov <skywalker855@gmail.com>
2021-11-16 19:51:10 +0200

    Add NavBar.js

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Context } from '..';
 import { authRoutes, publicRoutes } from '../routes';
+import { SHOP_ROUTE } from '../utils/consts';
 
 function AppRouter() {
-  const isAuth = false;
+  const { user, device } = useContext(Context);
+
+  console.log('user', device);
   return (
     <Routes>
-      {isAuth &&
+      {user.isAuth &&
         authRoutes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} exact />
         ))}
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} exact />
       ))}
+      <Route to={SHOP_ROUTE} />
     </Routes>
   );
 }
